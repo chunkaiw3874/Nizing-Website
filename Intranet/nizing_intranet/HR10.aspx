f17762cf45dcc44f9716322d5f5714d1ef82c3b4
chunkaiw3874 <chunkaiw3874@hotmail.com>
2018-08-16 14:50:10 +0800

    update

---
+++ Intranet/nizing_intranet/HR10.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/masterPage/MasterPage.master" AutoEventWireup="true" CodeFile="HR10.aspx.cs" Inherits="nizing_intranet_HR10" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <link href="../Content/bootstrap-datepicker3.css" rel="stylesheet" />    
+    <script src="../Scripts/bootstrap-datepicker.js"></script>
+    <script src="../Scripts/locales/bootstrap-datepicker.zh-TW.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('.datepicker').datepicker({
+                language: 'zh-TW',
+                format: 'yyyy-mm-dd',
+                autoclose: true,
+                todayBtn: true,
+                todayHighlight: true
+            });
+        })
+    </script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div class="row">
+        <div class="col-xs-12">
+            <h2>每月人事出勤首尾筆報告(由智碁系統擷取)</h2>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="row">
+            <div class="col-xs-2" style="display: inline; padding-right: 0px;">
+                <asp:TextBox ID="txtDatePickerStart" runat="server" CssClass="form-control datepicker" placeholder="選擇查詢開始日期"></asp:TextBox>
+            </div>            
+            <div class="col-xs-2" style="display: inline; padding-right: 0px;">
+                <asp:TextBox ID="txtDatePickerEnd" runat="server" CssClass="form-control datepicker" placeholder="選擇查詢結束日期"></asp:TextBox>
+            </div>
+            <div class="col-xs-2">
+                <asp:Button ID="btnSubmit" runat="server" Text="查詢" CssClass="btn btn-success" OnClick="btnSubmit_Click" />
+            </div>
+        </div>
+    </div>
+    <div class="row printarea">
+        <div class="col-xs-12">
+            <asp:GridView ID="gvResult" runat="server"></asp:GridView>
+        </div>
+    </div>
+</asp:Content>
+
---
+++ Intranet/nizing_intranet/HR10.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class nizing_intranet_HR10 : System.Web.UI.Page
+{
+    string stoneaxConnectionString = ConfigurationManager.ConnectionStrings["StoneaxConnectionString"].ConnectionString;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        DataSet ds = new DataSet();
+        DataTable dtAll = ds.Tables.Add();
+        DateTime searchStartTime = new DateTime();
+        DateTime searchEndTime = new DateTime();
+        DateTime result = new DateTime();
+        List<string> userIndex = new List<string>();
+
+        if (DateTime.TryParse(txtDatePickerStart.Text.Trim(), out result))
+        {
+            searchStartTime = result;
+        }
+        if (DateTime.TryParse(txtDatePickerEnd.Text.Trim(), out result))
+        {
+            searchEndTime = result;
+        }
+        using (SqlConnection conn = new SqlConnection(stoneaxConnectionString))
+        {
+            conn.Open();
+            string query = "CREATE TABLE #WORKTIMERECORD(RecordDate NVARCHAR(10), UserIndex Nvarchar(10), UserName Nvarchar(10), FirstRecordTime time, LastRecordTime time)"
+                        + " DECLARE @CURRENTUSERINDEX NVARCHAR(10)"
+                        + " DECLARE USERINDEX_CURSOR CURSOR FOR"
+                        + " SELECT DISTINCT(UserIndex)"
+                        + " FROM Record"
+                        + " WHERE RecordTime >= @STARTDATE AND RecordTime < @ENDDATE"
+                        + " ORDER BY UserIndex"
+                        + " WHILE (@STARTDATE < @ENDDATE)"
+                        + " BEGIN"
+                        + " 	OPEN USERINDEX_CURSOR"
+                        + " 	FETCH NEXT FROM USERINDEX_CURSOR"
+                        + " 	INTO @CURRENTUSERINDEX"
+                        + " 	WHILE @@FETCH_STATUS=0"
+                        + " 	BEGIN"
+                        + " 		INSERT INTO #WORKTIMERECORD"
+                        + " 		SELECT DISTINCT CONVERT(NVARCHAR(10),RecordTime,111), UserIndex, UserName"
+                        + " 		, (SELECT MIN(CAST(RecordTime AS Time)) FROM Record"
+                        + " 		WHERE UserIndex=@CURRENTUSERINDEX"
+                        + " 		AND CAST(RecordTime AS DATE) >= @STARTDATE"
+                        + " 		AND CAST(RecordTime AS DATE) < DATEADD(DAY,1,@STARTDATE))"
+                        + " 		,(SELECT MAX(CAST(RecordTime AS Time)) FROM Record"
+                        + " 		WHERE UserIndex=@CURRENTUSERINDEX"
+                        + " 		AND CAST(RecordTime AS DATE) >= @STARTDATE"
+                        + " 		AND CAST(RecordTime AS DATE) < DATEADD(DAY,1,@STARTDATE))"
+                        + " 		FROM Record"
+                        + " 		WHERE UserIndex=@CURRENTUSERINDEX"
+                        + " 		AND CAST(RecordTime AS DATE) >= @STARTDATE"
+                        + " 		AND CAST(RecordTime AS DATE) < DATEADD(DAY,1,@STARTDATE)"
+                        + " 		GROUP BY RecordTime,UserIndex,UserName,EventTypeIndex"
+                        + " 		FETCH NEXT FROM USERINDEX_CURSOR"
+                        + " 		INTO @CURRENTUSERINDEX"
+                        + " 	END"
+                        + " 	CLOSE USERINDEX_CURSOR	"
+                        + " 	SET @STARTDATE=DATEADD(DAY,1,@STARTDATE)"
+                        + " END"
+                        + " SELECT *"
+                        + " FROM #WORKTIMERECORD"
+                        + " ORDER BY UserIndex,RecordDate";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@STARTDATE", searchStartTime);
+            cmd.Parameters.AddWithValue("@ENDDATE", searchEndTime.AddDays(1));
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds.Tables[0]);
+        }
+        foreach (DataRow row in ds.Tables[0].Rows)
+        {
+            userIndex.Add(row["UserIndex"])
+        }
+        gvResult.DataSource = ds.Tables[0];
+        
+        gvResult.DataBind();
+    }
+}
