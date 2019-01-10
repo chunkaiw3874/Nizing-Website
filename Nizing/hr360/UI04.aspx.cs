6c3a8757e0cbe17801af94a1fa6e8374b46ac4d4
chunkaiw3874 <chunkaiw3874@hotmail.com>
2019-01-10 15:33:54 +0800

    update

--- Nizing/hr360/UI04.aspx.cs
+++ Nizing/hr360/UI04.aspx.cs
@@ -45,8 +45,8 @@ public partial class hr360_UI04 : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        Session["user_id"] = "0067";    //test only to avoid error on loading, delete after trial            
-        Session["erp_id"] = "0067";        
+        //Session["user_id"] = "0067";    //test only to avoid error on loading, delete after trial            
+        //Session["erp_id"] = "0067";        
 
         if (!((masterPage_HR360_Master)this.Master).CheckAuthentication())
         {
