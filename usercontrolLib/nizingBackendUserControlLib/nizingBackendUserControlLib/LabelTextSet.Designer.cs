ff4b452f0c8517a51bb7602dd4bebf9baf4ef10f
chunkaiw3874 <chunkaiw3874@hotmail.com>
2020-01-08 11:59:25 +0800

    new user control

---
+++ usercontrolLib/nizingBackendUserControlLib/nizingBackendUserControlLib/LabelTextSet.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace nizingBackendUserControlLib
+{
+    partial class LabelTextSet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AccessibleName = "";
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(0, 3);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(0, 12);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblTitle.SizeChanged += new System.EventHandler(this.LblTitle_SizeChanged);
+            // 
+            // txtContent
+            // 
+            this.txtContent.AccessibleName = "";
+            this.txtContent.Dock = System.Windows.Forms.DockStyle.Right;
+            this.txtContent.Location = new System.Drawing.Point(43, 0);
+            this.txtContent.Margin = new System.Windows.Forms.Padding(0);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(100, 22);
+            this.txtContent.TabIndex = 1;
+            // 
+            // LabelTextSet
+            // 
+            this.AccessibleName = "";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.lblTitle);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "LabelTextSet";
+            this.Size = new System.Drawing.Size(143, 22);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtContent;
+    }
+}
---
+++ usercontrolLib/nizingBackendUserControlLib/nizingBackendUserControlLib/LabelTextSet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nizingBackendUserControlLib
+{
+    public partial class LabelTextSet: UserControl
+    {
+        public LabelTextSet()
+        {
+            InitializeComponent();
+            lblTitle.Text = this.Name;
+        }
+
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public string Title 
+        {
+            get { return lblTitle.Text; }
+            set { lblTitle.Text = value; }
+        }
+
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public override string Text 
+        { 
+            get => txtContent.Text; 
+            set => txtContent.Text = value; 
+        }
+
+        private void LblTitle_SizeChanged(object sender, EventArgs e)
+        {
+            this.Width = lblTitle.Width + txtContent.Width;
+        }
+    }
+}
